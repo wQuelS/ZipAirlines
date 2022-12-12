86d26f6109a81cab7283f21c2e97a73427be8465
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-12 22:06:10 +0200

    Implement views for airplanes

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airplanes.models import Airplane
+from airplanes.serializers import AirplaneSerializer
+
+
+class AirplaneViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer

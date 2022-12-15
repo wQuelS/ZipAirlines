d313db443a0314aa0d1b873599e66bf488c8a9c5
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-16 01:08:31 +0200

    Reimplement models and serializers

--- airplanes/models.py
+++ airplanes/models.py
@@ -1,44 +1,39 @@
+from typing import Type
+
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 from math import log
 
+from rest_framework.exceptions import ValidationError
+
+
+class Airline(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Airplane(models.Model):
-    id = models.IntegerField(primary_key=True)
-    board_num = models.CharField(max_length=10, unique=True)
+    id = models.IntegerField(
+        primary_key=True, validators=[MinValueValidator(1)]
+    )
+    capacity = models.IntegerField(validators=[MinValueValidator(1)])
     passengers = models.IntegerField()
-    fuel_tank = models.IntegerField(blank=True)
-    consumption_per_minute = models.DecimalField(
-        max_digits=7, decimal_places=5, blank=True
-    )
-    minutes_to_fly = models.DecimalField(
-        max_digits=7, decimal_places=2, blank=True, null=True
+    airline = models.ForeignKey(
+        Airline, on_delete=models.CASCADE, related_name="airplanes", default=1
     )
 
-    @property
-    def _get_tank_capacity(self) -> int:
-        return self.id * 200
+    def clean(self) -> None:
+        if self.passengers > self.capacity:
+            raise ValidationError(
+                "The number of passengers cannot be greater than the capacity of the airplane."
+            )
 
-    @property
-    def _get_consumption_per_minute(self) -> float:
-        consumption_multiplier = 0.80
-        passenger_consumption_rate = 0.002
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
-        consumption = log(self.id) * consumption_multiplier
-
-        if self.passengers >= 1:
-            consumption += self.passengers * passenger_consumption_rate
-        return round(consumption, 5)
-
-    @property
-    def _get_minutes_to_fly(self) -> float:
-        minutes = self.fuel_tank / self.consumption_per_minute
-        return round(minutes, 2)
-
-    def save(self, *args, **kwargs) -> None:
-        self.fuel_tank = self._get_tank_capacity
-        self.consumption_per_minute = self._get_consumption_per_minute
-        self.minutes_to_fly = self._get_minutes_to_fly
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.board_num
+    def __str__(self) -> str:
+        return f"{self.airline.name} (id: {self.id}, capacity: {self.capacity}, passenger: {self.passengers})"
--- airplanes/serializers.py
+++ airplanes/serializers.py
@@ -1,16 +1,89 @@
-from rest_framework import serializers
+from typing import Dict, Any
 
-from airplanes.models import Airplane
+from django.conf import settings
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
+
+from airplanes.models import Airplane, Airline
+from airplanes.airplane_utility import AirplaneUtility
+
+
+# class AirlineSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = Airline
+#         fields = ("id", "name")
 
 
 class AirplaneSerializer(serializers.ModelSerializer):
+    tank_capacity = serializers.SerializerMethodField()
+    consumption_per_minute = serializers.SerializerMethodField()
+    minutes_to_fly = serializers.SerializerMethodField()
+    airline = serializers.SerializerMethodField()
+
+    def get_airline(self, obj: Airplane) -> str:
+        return obj.airline.name
+
     class Meta:
         model = Airplane
         fields = (
             "id",
-            "board_num",
+            "airline",
             "passengers",
+            "capacity",
+            "tank_capacity",
             "consumption_per_minute",
             "minutes_to_fly",
         )
-        read_only_fields = ("consumption_per_minute", "minutes_to_fly")
+        read_only_fields = (
+            "consumption_per_minute",
+            "minutes_to_fly",
+            "fuel_tank",
+        )
+
+    def get_tank_capacity(self, plane: Airplane) -> int:
+        return AirplaneUtility(plane).tank_capacity
+
+    def get_consumption_per_minute(self, plane: Airplane) -> float:
+        return AirplaneUtility(plane).consumption_per_minute
+
+    def get_minutes_to_fly(self, plane: Airplane) -> float:
+        return AirplaneUtility(plane).minutes_to_fly
+
+
+class AirplaneCreateSerializer(AirplaneSerializer):
+    def validate(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        if Airplane.objects.count() >= 10:
+            raise serializers.ValidationError(
+                "Cannot create more than 10 airplanes."
+            )
+
+        return data
+
+    #
+    # def create(self, validated_data: Dict[str, Any]) -> Airplane:
+    #     # Create a new instance of the Airplane model using the validated data
+    #     airplane = Airplane.objects.create(**validated_data)
+    #
+    #     # Return the newly created airplane
+    #     return airplane
+
+
+class AirlineSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    name = serializers.CharField(max_length=100, required=False)
+    airplanes = AirplaneSerializer(
+        many=True, max_length=settings.MAX_PLANES, required=False
+    )
+
+    class Meta:
+        fields = (
+            "id",
+            "name",
+            "airplanes",
+        )
+
+    def get_airplanes(self, airline: Airplane) -> Airplane:
+        # Return the queryset of airplanes for the given airline
+        return Airplane.objects.filter(airline=airline)

a34ecd4181a6ca6405f9960d643907b3db82713d
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-16 01:10:38 +0200

    Implement views, update urls

--- airplanes/urls.py
+++ airplanes/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airplanes.views import AirplaneViewSet
+from airplanes.views import AirplaneViewSet, fleet
 
 router = routers.DefaultRouter()
 router.register("airplanes", AirplaneViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("airline/", fleet, name="airlines"),
+]
 
 app_name = "airplanes"
--- airplanes/views.py
+++ airplanes/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
-from rest_framework import mixins, viewsets
-from rest_framework.viewsets import GenericViewSet
+from typing import Type
 
-from airplanes.models import Airplane
-from airplanes.serializers import AirplaneSerializer
+from django.db.migrations import serializer
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from airplanes.models import Airplane, Airline
+from airplanes.serializers import (
+    AirplaneSerializer,
+    AirlineSerializer,
+    AirplaneCreateSerializer,
+)
 
 
 class AirplaneViewSet(
-    viewsets.ModelViewSet,
+    mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet
 ):
     queryset = Airplane.objects.all()
-    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action == "create":
+            return AirplaneCreateSerializer
+        return AirplaneSerializer
+
+
+@api_view(["GET"])
+def fleet(request) -> Response:
+    airline = Airline.objects.get()
+    request.data["airline"] = airline
+
+    serializer = AirlineSerializer(data=request.data, instance=airline)
+
+    if serializer.is_valid(raise_exception=True):
+        return Response(serializer.data)
